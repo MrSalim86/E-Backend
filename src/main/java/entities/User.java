89dc93b2d4d23c85c31f2a6cbaf151d66fe8675f
MrSalim86 <cph-ms1068@cphbusiness.dk>
2023-06-04 22:58:18 +0200

    make user entite

--- src/main/java/entities/User.java
+++ src/main/java/entities/User.java
@@ -31,6 +31,71 @@ public class User implements Serializable {
   @ManyToMany
   private List<Role> roleList = new ArrayList<>();
 
+  @Column(name = "address")
+  private String address;
+
+  @Column(name = "phone")
+  private String phone;
+
+  @Column(name = "email")
+  private String email;
+
+  @Column(name = "birth_year")
+  private Integer birth_year;
+
+  @Column(name = "gender")
+  private String gender;
+
+
+
+  public User(String address, String phone, String email, Integer birth_year, String gender) {
+    this.address = address;
+    this.phone = phone;
+    this.email = email;
+    this.birth_year = birth_year;
+    this.gender = gender;
+  }
+
+  public String getGender() {
+    return gender;
+  }
+
+  public void setGender(String gender) {
+    this.gender = gender;
+  }
+
+  public Integer getBirth_year() {
+    return birth_year;
+  }
+
+  public void setBirth_year(Integer birth_year) {
+    this.birth_year = birth_year;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getPhone() {
+    return phone;
+  }
+
+  public void setPhone(String phone) {
+    this.phone = phone;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public void setAddress(String address) {
+    this.address = address;
+  }
+
   public List<String> getRolesAsStrings() {
     if (roleList.isEmpty()) {
       return null;
